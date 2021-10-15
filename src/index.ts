5d27951c127f50567a8971a0f5e37ed1ef97698c
Jona Wessendorf <jonawessendorf@gmail.com>
2021-10-15 13:24:49 +0200

    adds function to get the links and names for all the Units

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
+import * as cheerio from 'cheerio';
 
 dotenv.config();
 
@@ -17,7 +18,77 @@ async function getPageContent() {
     auth,
   });
 
-  console.log(response.data);
+  return response.data as string;
 }
 
-getPageContent();
+async function getVorlesungsWochen(): Promise<VorlesungsWoche[]> {
+  const pageContent = await getPageContent();
+
+  const $ = cheerio.load(pageContent);
+
+  const mainDiv = $('.col2');
+
+  const vorlesungsWochen: VorlesungsWoche[] = [];
+  let week = 0;
+  let vorlesungsWoche: VorlesungsWoche = new VorlesungsWoche(week);
+  let name: string;
+  let mp4URL: string;
+  let pdfURL: string;
+
+  const reset = () => {
+    name = '';
+    mp4URL = '';
+    pdfURL = '';
+  };
+
+  reset();
+
+  mainDiv.children().each((i, el) => {
+    const element = $(el);
+    const elementType = element[0].name;
+    const elementText = element.text();
+    const href = element.attr('href');
+
+    // checks if a new VorlesungsWoche begins or the last one has ended
+    if (elementType === 'h2' || i === mainDiv.children().length - 1) {
+      week++;
+      vorlesungsWochen.push(vorlesungsWoche);
+      vorlesungsWoche = new VorlesungsWoche(week);
+      reset();
+    }
+    if (week === 0) return;
+
+    if (elementType === 'b') name = element.text();
+    if (elementType === 'a' && href) {
+      if (elementText.includes('[MP4]')) {
+        mp4URL = href;
+      } else if (elementText.includes('[PDF]')) {
+        pdfURL = href;
+
+        vorlesungsWoche.units.push({ name, mp4URL, pdfURL });
+
+        reset();
+      }
+    }
+  });
+
+  return vorlesungsWochen.slice(1);
+}
+
+getVorlesungsWochen();
+
+class VorlesungsWoche {
+  nr: number;
+  units: Unit[];
+
+  constructor(nr: number) {
+    this.nr = nr;
+    this.units = [];
+  }
+}
+
+interface Unit {
+  name: string;
+  mp4URL: string;
+  pdfURL: string;
+}
