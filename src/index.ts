1396633a8bbcf7b8cd1b45bb5c16819458cfdc62
Jona Wessendorf <jonawessendorf@gmail.com>
2021-10-15 12:19:55 +0200

    add loading of the page content

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('hello world');
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const URL = process.env.URL;
+
+if (!username || !password || !URL)
+  throw Error('username, password and URL are required in the .env file!');
+
+const auth = { username, password };
+
+async function getPageContent() {
+  const response = await axios.get(URL!, {
+    auth,
+  });
+
+  console.log(response.data);
+}
+
+getPageContent();
